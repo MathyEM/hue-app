72b7c1b1736ed4b229f742caf81dd89abb1282e4
MathyEM <72448225+MathyEM@users.noreply.github.com>
2021-04-08 15:51:20 +0200

    Added HueEntity to App.vue and added initial state getter for hue api

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png"><br>
+    <div v-for="light, index in lights" :key="index">
+      <HueEntity :lightName="light.name" :state="light.state" :id="index"/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import store from './store'
+import axios from 'axios'
+import HueEntity from './components/HueEntity.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HueEntity
+  },
+  data() {
+    return {
+
+    };
+  },
+  computed: {
+    lights() {
+      return store.state.lights;
+    }
+  },
+  async mounted() {
+    const response = await axios.get(`http://${process.env.VUE_APP_HUE_BRIDGE_IP}/api/${process.env.VUE_APP_HUE_USERNAME}/lights`);
+    store.commit('updateLights', response.data);
+    console.log(store.state.lights);
+  },
+  
 }
 </script>
 
