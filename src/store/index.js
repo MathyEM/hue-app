2ab36d8095e1c41b8906e60b7b8dd1896a1d5578
MathyEM <72448225+MathyEM@users.noreply.github.com>
2021-04-08 15:49:05 +0200

    Added lights and update lights to store.state and store.mutations

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,13 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    lights: {}
   },
   mutations: {
+    updateLights(state, payload) {
+      state.lights = payload
+    },
+
   },
   actions: {
   },
