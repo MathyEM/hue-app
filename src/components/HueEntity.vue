5e7088576434120b285c614e8e66d14c84e928e0
MathyEM <72448225+MathyEM@users.noreply.github.com>
2021-04-11 17:01:25 +0200

    Radial color picker and saturation range now changes lamp color

--- src/components/HueEntity.vue
+++ src/components/HueEntity.vue
@@ -3,8 +3,8 @@
         <h3 class="lamp-title">{{ id }}: {{ lightName }}</h3>
         <p class="lamp-info">On: {{ state.on }}</p>
         <div class="color-picker-wrapper">
-            <ColorPicker class="color-picker" v-bind="color" @input="onInput"></ColorPicker>
-            <input type="range" class="saturation" name="saturation" id="saturation" min="0" max="254" v-model="state.sat">
+            <ColorPicker class="color-picker" v-bind="color" @change="onHueInput"></ColorPicker>
+            <input type="range" class="saturation" name="saturation" id="saturation" min="0" max="254" :value="satCalcPhilips(color.saturation)" @change="onSatInput">
         </div>
         <HueDimmerSwitch :id="id" :state="state" :controlLight="controlLight" />
     </div>
@@ -16,6 +16,8 @@ import axios from 'axios'
 import ColorPicker from '@radial-color-picker/vue-color-picker'
 import HueDimmerSwitch from './HueDimmerSwitch'
 import tinycolor from "tinycolor2"
+const converter = require('@q42philips/hue-color-converter');
+
 
 export default {
     name: 'HueEntity',
@@ -48,61 +50,115 @@ export default {
         }
     },
     methods: {
-        getHSL() {
-            const hue = this.hueCalcPercent();
-            const sat = this.satCalc();
-            const bri = this.briCalc();
+        getColors() { // Get HSL color for the radial-color-picker
+            const hue = this.hueCalcPercent(this.$props.state.hue);
+            const sat = this.satCalcPercent(this.$props.state.sat);
+            const bri = this.briCalcPercent(this.$props.state.bri);
 
             const color = tinycolor(`hsv(${hue}%,${sat}%,${bri}%)`);
 
-            const hsl = color.toHsl();
-            const hsv = color.toHsv();
+            const hsl = color.toHsl(); // For radial-color-picker
+            const hsv = color.toHsv(); // For Hue lights
 
-            this.color.hue = this.hueCalcRadial();
-            this.color.saturation = hsl.s*100;
-            this.color.luminosity = hsl.l*100;
-
-            console.log("HSL:", hsl);
-            console.log("HSV:", hsv);
+            return {hsl, hsv};
         },
-        hueCalcPercent() {
-            const pHue = this.$props.state.hue;
+        setHSL() { // Set the color for the radial-color-picker
+            this.color.hue = this.hueCalcRadial(this.$props.state.hue);
+            this.color.saturation = this.getColors().hsl.s*100;
+            this.color.luminosity = this.getColors().hsl.l*100;
+        },
+        hueCalcPercent(hue) { // Used for converting Philips Hue's hue-scale to a 100% scale
+            const pHue = hue;
             const pHueMax = 65535;
             const hueMax = 100;
             const hueRatio = hueMax/pHueMax;
 
             return pHue*hueRatio
         },
-        hueCalcRadial() {
-            const pHue = this.$props.state.hue;
+        hueCalcRadial(hue) { // Used for converting Philips Hue's hue-scale to a 359deg scale
+            const pHue = hue;
             const pHueMax = 65535;
             const hueMax = 359;
             const hueRatio = hueMax/pHueMax;
 
             return pHue*hueRatio
         },
-        satCalc() {
-            const pSat = this.$props.state.sat;
+        hueCalcPhilips(hue) { // Used for converting 100% hue-scale to Philips Hue's scale
+            const pHue = hue;
+            const pHueMax = 65535;
+            const hueMax = 100;
+            const hueRatio = pHueMax/hueMax;
+
+            return pHue*hueRatio
+        },
+        hueCalcRadialToPercent(hue) { // Used for converting 100% hue-scale to Philips Hue's scale
+            const pHue = hue;
+            const pHueMax = 359;
+            const hueMax = 100;
+            const hueRatio = hueMax/pHueMax;
+
+            return pHue*hueRatio
+        },
+        satCalcPercent(sat) { // Used for converting Philips Hue's saturation scale to a 100% scale
+            const pSat = sat;
             const pSatMax = 254;
             const satMax = 100;
             const satRatio = satMax/pSatMax;
 
             return pSat*satRatio;
         },
-        briCalc() {
-            const pBri = this.$props.state.bri;
+        satCalcPhilips(sat) { // Used for converting 100% saturation scale to Philips Hue's scale
+            const pSat = sat;
+            const pSatMax = 254;
+            const satMax = 100;
+            const satRatio = pSatMax/satMax;
+
+            return pSat*satRatio;
+        },
+        briCalcPercent(bri) { // Used for converting Philips Hue's brightness scale to a 100% scale
+            const pBri = bri;
             const pBriMax = 254;
             const briMax = 100;
             const briRatio = briMax/pBriMax;
 
             return pBri*briRatio;
         },
-        onInput(hue) {
-            this.color.hue = hue;
+        briCalcPhilips(bri) { // Used for converting 100% brightness scale to Philips Hue's scale
+            const pBri = bri;
+            const pBriMax = 254;
+            const briMax = 100;
+            const briRatio = pBriMax/briMax;
+
+            return pBri*briRatio;
         },
-        async controlLight (lightId, on, bri, hue, sat) {
+        onHueInput(hue) {
+            this.color.hue = hue;
+
+            // Control the lights
+            const color = tinycolor("hsl("+this.hueCalcRadialToPercent(this.color.hue)+"%, "+this.color.saturation+"%, "+this.color.luminosity+"%)");
+            const rgbColor = color.toRgb();
+            const xyColor = converter.calculateXY(rgbColor.r, rgbColor.g, rgbColor.b, "LCA001");
+
+            console.log(color, rgbColor, xyColor);
+
+            this.controlLightXY(this.$props.id, true, xyColor);
+        },
+        onSatInput(e) {
+            // Change lights locally
+            const colorSat = e.target.value;
+            this.color.saturation = this.satCalcPercent(colorSat);
+
+            // Control the lights
+            const color = tinycolor("hsl("+this.hueCalcRadialToPercent(this.color.hue)+"%, "+this.color.saturation+"%, "+this.color.luminosity+"%)");
+            const rgbColor = color.toRgb();
+            const xyColor = converter.calculateXY(rgbColor.r, rgbColor.g, rgbColor.b, "LCA001");
+
+            console.log(color, rgbColor, xyColor);
+
+            this.controlLightXY(this.$props.id, true, xyColor);
+        },
+        async controlLight(lightId, on, bri, hue, sat) {
             try {
-                let self = this;
                 return await axios.put(
                     `http://${process.env.VUE_APP_HUE_BRIDGE_IP}/api/${process.env.VUE_APP_HUE_USERNAME}/lights/${lightId}/state`,
                     {
@@ -113,7 +169,6 @@ export default {
                     }
                 ).then(function () {
                     let temp = store.state.lights;
-                    self.getHSL()
                     if (on !== undefined) {
                         temp[lightId].state.on = on;
                     }
@@ -134,10 +189,38 @@ export default {
                 console.log(error);
             }
         },
+        async controlLightXY(lightId, on, xy, bri) {
+            try {
+                return await axios.put(
+                    `http://${process.env.VUE_APP_HUE_BRIDGE_IP}/api/${process.env.VUE_APP_HUE_USERNAME}/lights/${lightId}/state`,
+                    {
+                        on,
+                        ...(xy && { xy }),
+                        ...(bri && { bri }),
+                    }
+                ).then(function () {
+                    let temp = store.state.lights;
+                    if (on !== undefined) {
+                        temp[lightId].state.on = on;
+                    }
+                    if (xy !== undefined) {
+                        temp[lightId].state.xy = xy;
+                    }
+                    if (bri !== undefined) {
+                        temp[lightId].state.bri = bri;
+                    }
+
+                    store.commit('updateLights', temp);
+                    console.log(store.state.lights[lightId].state);
+                })
+            } catch (error) {
+                console.log(error);
+            }
+        },
 
     },
     async mounted() {
-        this.getHSL();
+        this.setHSL();
     }
 }
 </script>
