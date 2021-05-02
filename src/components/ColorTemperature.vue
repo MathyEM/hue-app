22ed5b97a625f312f3839f02af4855138c0b2c76
MathyEM <72448225+MathyEM@users.noreply.github.com>
2021-05-02 14:00:24 +0200

    Color temp sliders now shows the color gradient from which you can chose

--- src/components/ColorTemperature.vue
+++ src/components/ColorTemperature.vue
@@ -50,8 +50,43 @@ export default {
 </script>
 
 <style lang="scss">
+$thumb-width: 14px;
+$thumb-height: 28px;
+$track-height: 10px;
+$thumb-margin-top: ( $thumb-height * -0.5 ) + ( $track-height * 0.5 );
+
+* {
+	--thumb-color: whitesmoke;
+	--test: #{$thumb-margin-top};
+}
+
 .color-temperature {
 	position: relative; // This makes the slider clickable even when the color wheel is overlapping
 	padding: 1rem 0;
+	-webkit-appearance: none;
+	background: transparent;
+
+	&:focus {
+		outline: none;
+	}
+
+	&::-webkit-slider-thumb {
+		-webkit-appearance: none;
+		border: 1px solid lightgray;
+		width: $thumb-width;
+		height: $thumb-height;
+		border-radius: 3px;
+		background: var(--thumb-color);
+		cursor: pointer;
+		justify-self: center;
+		margin-top: $thumb-margin-top; /* You need to specify a margin in Chrome, but in Firefox and IE it is automatic */
+	}
+	&::-webkit-slider-runnable-track {
+		cursor: pointer;
+		height: $track-height;
+		border: 1px solid lightgray;
+		border-radius: 2px;
+		background: linear-gradient(90deg, #b1c1ff, #d4e0ff, #fff8cf, #f8fcff, #ffeb9b, #ffdd6f, #ffcf4d, #ffc031, #ffb11d);
+	}
 }
 </style>
