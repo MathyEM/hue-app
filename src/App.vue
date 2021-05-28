20265548d24f780a341d3ca4c3af6b6e60ff3127
MathyEM <72448225+MathyEM@users.noreply.github.com>
2021-05-28 12:10:18 +0200

    Removed Vue logo header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
 	<div id="app">
-		<img alt="Vue logo" src="./assets/logo.png">
 		<div class="container">
 			<GroupList />
 			<template v-for="group, g_index in groups">
@@ -93,7 +92,7 @@ export default {
 	-moz-osx-font-smoothing: grayscale;
 	text-align: center;
 	color: #2c3e50;
-	margin-top: 60px;
+	margin-top: 20px;
 
 	.container {
 		display: flex;
